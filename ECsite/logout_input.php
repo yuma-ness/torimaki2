c2c5048325db08b196bafdc90d076785fa8a344d
KoyamaIssei <2201145@s.asojuku.ac.jp>
2023-10-27 10:13:15 +0900

    aaaa

--- ECsite/logout_input.php
+++ ECsite/logout_input.php
@@ -1,5 +1,12 @@
+<link rel="stylesheet" href="css/logout-input.css">
+<div id='back'>
+    <h1>ログアウト</h1>
+</div>
+<div id='a'>
 <?php require 'header.php'; ?>
-<p>ログアウトしますか？</p>
-<a href="logout_output.php">ログアウト</a>
-<a href="index.php" id="my">マイページへ</a>
-<?php require 'footer.php'; ?>  
+<p class='log'>ログアウトします本当によろしいですか？</br>
+ログアウト後のログイン画面へ移行します</p>
+</div>
+<a href="logout_output.php"><button>ログアウト</button></a>
+<a href="index.php" id="my"><button>マイページへ</button></a>
+<?php require 'footer.php'; ?>
