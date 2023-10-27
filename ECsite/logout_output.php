85cc50bb2c68f59de5aa62963d68f4865c9df565
KoyamaIssei <2201145@s.asojuku.ac.jp>
2023-10-27 10:11:35 +0900

    aaa

--- ECsite/logout_output.php
+++ ECsite/logout_output.php
@@ -1,13 +1,22 @@
 <?php session_start(); ?>
 <?php require 'header.php'; ?>
+<link rel="stylesheet" href="css/logout-output.css">
+<div id='back'>
+    <h1>ログアウト</h1>
+    <div class="i">
+    <img src ="image/rogo.jpg">
+</div>
+</div>
+<div id='a'>
 <?php
 if(isset($_SESSION['customer'])){
     unset($_SESSION['customer']);
-    echo 'ログアウトしました。';
-    echo '<a href="index.php" id="my">マイページへ</a>';
+    echo '<p class="log">ログアウトしました。</p>';
 }else{
-    echo '既にログアウトしています。';
-    echo '<a href="index.php" id="my">マイページへ</a>';
+    echo '<p class="log">既にログアウトしています。</p>';
 }
 ?>
+
+</div>
+    <a href="login_inout.php" id="my"><button>ログインへ戻る</button></a>
 <?php require 'footer.php'; ?>
