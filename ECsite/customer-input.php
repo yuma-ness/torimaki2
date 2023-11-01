0322903378636aa49c2763f594f81e341bdddacf
gotoriku0625 <2201147@s.asojuku.ac.jp>
2023-11-01 11:23:47 +0900

    a

--- ECsite/customer-input.php
+++ ECsite/customer-input.php
@@ -5,6 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+<div class="flex">
+<figure class="image"><img 
+src="image/rogo.jpg">
+</figure>
+<h1>新規登録<h1>
+</div>
 <body>
     <?php
     $name=$password=$address=$useraddress='';
