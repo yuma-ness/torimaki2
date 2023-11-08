021e35e813399df916a150832508e0e60d746265
masaya200104 <shimatatematate@gmail.com>
2023-11-08 12:15:18 +0900

    Update customer-output.php

--- ECsite/customer-output.php
+++ ECsite/customer-output.php
@@ -46,11 +46,11 @@
                 echo'<p class="log">お客様情報を登録しました。</p>';
                 echo '<a href="login_input.php" id="my"><button>ログインへ戻る</button></a>';
     }else{
-        echo'<p class="log">ログイン名が既に使用されていますので、変更してください。</p>';
+        echo'<p class="log">ログイン名が既に使用されています。</p>';
         echo '<a href="customer_input.php" id="my"><button>登録画面へ戻る</button></a>';
     }
     }else{
-        echo'<p class="log">メールアドレスが既に使用されていますので、変更してください。</p>';
+        echo'<p class="log">メールアドレスが既に使用されています。</p>';
         echo '<a href="customer_input.php" id="my"><button>登録画面へ戻る</button></a>';
     }
 ?>
