bc3666407b0ebdcd202212d3166ad0a8f534fc4b
masaya200104 <shimatatematate@gmail.com>
2023-11-24 11:52:48 +0900

    a

--- ECsite/cart-show.php
+++ ECsite/cart-show.php
@@ -1,11 +1,22 @@
 <?php session_start(); ?>
-<?php require 'header.php'; ?>
-<link rel="stylesheet" href="css/cart.css">
-<link rel="stylesheet" href="css/cart-show.css">
-<div id="back">
-    <div id="link">
-    <h1>カート</h1>
-    </div>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/cart-show.css?v=1.0.0">
+    <link rel="stylesheet" href="css/cart.css?v=1.0.1">
+    <title>Document</title>
+</head>
+<body>
+<div class="flex">
+<figure class="image">
+<img src ="image/rogo.jpg" class="hed">
+</figure>
+<h1>カート</h1>
+<div id="div">
+<a href="mypage.php" id="hi">マイページへ</a>
+</div>
 </div>
 <?php require 'cart.php'; ?>
 <?php require 'footer.php'; ?>
