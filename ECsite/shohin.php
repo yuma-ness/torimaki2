c33f87eb0d3dcc7dfc7df5376f19119860b7c7b5
KoyamaIssei <2201145@s.asojuku.ac.jp>
2023-12-07 23:44:31 +0900

    a

--- ECsite/shohin.php
+++ ECsite/shohin.php
@@ -1,5 +1,5 @@
 <?php require 'header.php'; ?>
-<link rel="stylesheet" href="css/shohin.css">
+<link rel="stylesheet" href="css/shohin.css?v=1.0.0">
 <?php require 'db-connect.php'; ?>
 <div class="flex">
     <figure class="image">
@@ -10,17 +10,26 @@
     <input type="text" name="kensaku" placeholder="商品名を検索できます" size="70" ><input type="submit" value="検索" size="35" >
     </form>
     </div>
-<div id="div">   
-        <a href="mypage.php" id="hi">マイページへ</a>
-        <br>
-        <a href="favorite-show.php" id="favo">お気に入りへ</a>
-        <br>
-        <a href="shohin_top.php" id="ab">商品トップへ</a>
-        <br>
-        <a href="logout_input.php">ログアウト</a>
-        <br>
-        <a href="cart-show.php">カート</a>
-</div>
+    <div class="nav">
+    
+    <!-- ハンバーガーメニューの表示・非表示を切り替えるチェックボックス -->
+    <input id="drawer_input" class="drawer_hidden" type="checkbox">
+
+    <!-- ハンバーガーアイコン -->
+    <label for="drawer_input" class="drawer_open"><span></span></label>
+
+    <!-- メニュー -->
+    <nav class="nav_content">
+      <ul class="nav_list">
+        <li class="nav_item"><a href="mypage.php" id="b">マイページへ</a></li>
+        <li class="nav_item"><a href="favorite-show.php" id="b">お気に入りへ</a></li>
+        <li class="nav_item"><a href="shohin_top.php" id="b">商品一覧</a></li>
+        <li class="nav_item"><a href="cart-show.php" id="b">カート</a></li>
+        <li class="nav_item"><a href="logout_input.php" id="b">ログアウト</a></li>
+      </ul>
+    </nav>
+
+  </div>
 </div>
 
 
