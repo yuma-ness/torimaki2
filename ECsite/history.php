f947e96de543009355832cef9bea63f1a1b96185
gotoriku0625 <2201147@s.asojuku.ac.jp>
2023-12-08 12:27:06 +0900

    a

--- ECsite/history.php
+++ ECsite/history.php
@@ -6,17 +6,25 @@
 <img src ="image/rogo.jpg">
 </figure>
     <h1>注文履歴</h1>
-    <div id="div">   
-        <a href="mypage.php" id="hi">マイページへ</a>
-        <br>
-        <a href="favorite-show.php" id="favo">お気に入りへ</a>
-        <br>
-        <a href="shohin_top.php" id="ab">商品トップへ</a>
-        <br>
-        <a href="logout_input.php">ログアウト</a>
-        <br>
-        <a href="cart-show.php">カート</a>  
-    </div>
+    <div class="nav">
+    
+    <!-- ハンバーガーメニューの表示・非表示を切り替えるチェックボックス -->
+    <input id="drawer_input" class="drawer_hidden" type="checkbox">
+
+    <!-- ハンバーガーアイコン -->
+    <label for="drawer_input" class="drawer_open"><span></span></label>
+
+    <!-- メニュー -->
+    <nav class="nav_content">
+      <ul class="nav_list">
+        <li class="nav_item"><a href="mypage.php" id="b">マイページへ</a></li>
+        <li class="nav_item"><a href="favorite-show.php" id="b">お気に入りへ</a></li>
+        <li class="nav_item"><a href="shohin_top.php" id="b">商品トップへ</a></li>
+        <li class="nav_item"><a href="cart-show.php" id="b">カート</a></li>
+      </ul>
+    </nav>
+
+  </div>
     </div>
 <?php
 $dayopen=0;
