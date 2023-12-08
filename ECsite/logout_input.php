f237bc89a9f211f11660cf467498164428d9eb78
gotoriku0625 <2201147@s.asojuku.ac.jp>
2023-12-08 12:18:17 +0900

    a

--- ECsite/logout_input.php
+++ ECsite/logout_input.php
@@ -3,7 +3,26 @@
 <figure class="image"><img 
 src="image/rogo.jpg">
 </figure>
-<h1>ログアウト<h1>
+<h1>ログアウト</h1>
+<div class="nav">
+    
+    <!-- ハンバーガーメニューの表示・非表示を切り替えるチェックボックス -->
+    <input id="drawer_input" class="drawer_hidden" type="checkbox">
+
+    <!-- ハンバーガーアイコン -->
+    <label for="drawer_input" class="drawer_open"><span></span></label>
+
+    <!-- メニュー -->
+    <nav class="nav_content">
+      <ul class="nav_list">
+        <li class="nav_item"><a href="mypage.php" id="b">マイページへ</a></li>
+        <li class="nav_item"><a href="favorite-show.php" id="b">お気に入りへ</a></li>
+        <li class="nav_item"><a href="shohin_top.php" id="b">商品一覧</a></li>
+        <li class="nav_item"><a href="cart-show.php" id="b">カート</a></li>
+      </ul>
+    </nav>
+
+  </div>
 </div>
 <div id='a'>
 <?php require 'header.php'; ?>
@@ -11,5 +30,4 @@ src="image/rogo.jpg">
 ログアウト後のログイン画面へ移行します</p>
 </div>
 <a href="logout_output.php"><button>ログアウト</button></a>
-<a href="mypage.php" id="my"><button>マイページへ</button></a>
 <?php require 'footer.php'; ?>
